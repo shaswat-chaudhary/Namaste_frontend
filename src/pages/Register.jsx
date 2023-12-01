4cedaf2de2fd786e86a835b00e8e2093440355b1
iamshaswat <shaswatkumar281@bbdu.ac.in>
2023-12-02 01:04:28 +0530

    changes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -52,11 +52,11 @@ export const Register = () => {
   return (
     <div className='bg-slate-800 w-full h-[100vh] flex items-center justify-center p-6'>
 
-      <div className='w-full md:w-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex flex-row-reverse bg-  overflow-hidden shadow-xl'>
+      <div className='w-full md:w-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex flex-row-reverse bg-  overflow-hidden shadow-xl border rounded-xl'>
 
         {/* Right SIDE  */}
 
-        <div className='w-full lg:w-1/2 h-full p-10 2xl:px-20 flex flex-col justify-center border-r border-y rounded-r-xl'>
+        <div className='w-full lg:w-1/2 h-full p-10 2xl:px-20 flex flex-col justify-center'>
 
           <div className='w-full flex gap-2 items-center mb-4'>
             <img src={logo}
